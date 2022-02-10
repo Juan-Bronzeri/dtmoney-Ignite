5158b7ef104fa6d68aebe9f144056d408f6a9e57
Juan Dias <juan.dias@texs.com.br>
2022-02-10 12:28:43 -0300

    feat: dashboard

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,17 @@
+import Modal from 'react-modal'
+
+interface INewTransactionModal {
+	isOpen: boolean
+	onRequestClose: () => void
+}
+
+export function NewTransactionModal({
+	isOpen,
+	onRequestClose,
+}: INewTransactionModal) {
+	return (
+		<Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+			<h2>Cadastrar transação</h2>
+		</Modal>
+	)
+}
